3511802b641e588807e89edb1c351eba17385923
VPRCSUF <royrosevin@csu.fullerton.edu>
2021-03-25 12:46:16 -0700

    Added a button to view the user's courses. Created the activity for the list of courses. Did not add the list yet.

--- app/src/main/java/com/example/studentassistantapplication/MainActivity.java
+++ app/src/main/java/com/example/studentassistantapplication/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.studentassistantapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +24,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-
-
-
+    public void goToMyCourses(View v)
+    {
+        Intent i = new Intent(this, MyCourses.class);
+        startActivity(i);
+    }
 
 }
+
