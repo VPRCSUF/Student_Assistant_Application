d34554dfa66251e561e8aaf63e1522a95d8f1372
n-hranuelli <79286394+n-hranuelli@users.noreply.github.com>
2021-03-30 21:15:33 -0700

    Added insert data function
    Made sure program can compile and run. Added an insert data function, however data insertion fails so need to debug that.

--- app/src/main/java/com/example/studentassistantapplication/ClassDatabaseHelper.java
+++ app/src/main/java/com/example/studentassistantapplication/ClassDatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.studentassistantapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,12 +11,13 @@ public class ClassDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "CourseInfo.db";
     public static final String TABLE_NAME = "Course_table";
-    public static final String COL_1_COURSE_NAME = "COURSE";
-    public static final String COL_2_PROFESSOR = "PROFESSOR";
-    public static final String COL_3_UNITS = "UNITS";
-    public static final String COL_4_START_TIME = "START TIME";
-    public static final String COL_5_END_TIME = "END_TIME";
-    public static final String COL_6_DAYS = "DAYS";
+    public static final String COL_1_ID = "ID";
+    public static final String COL_2_COURSE_NAME = "COURSE";
+    public static final String COL_3_PROFESSOR = "PROFESSOR";
+    public static final String COL_4_UNITS = "UNITS";
+    public static final String COL_5_START_TIME = "START TIME";
+    public static final String COL_6_END_TIME = "END_TIME";
+    public static final String COL_7_DAYS = "DAYS";
 
 
 
@@ -28,7 +30,7 @@ public class ClassDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-        db.execSQL("create table " + TABLE_NAME + " (Course text PRIMARY KEY, Professor text, Units integer, Start_time text, End_time text, Days text) ");
+        db.execSQL("create table " + TABLE_NAME + " (Id integer PRIMARY KEY AUTOINCREMENT, Course text, Professor text, Units integer, Start_time text, End_time text, Days text)");
     }
 
     @Override
@@ -37,5 +39,27 @@ public class ClassDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
 
+    }
+
+    public boolean insertData(String course, String professor, String units, String start, String end, String days)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_2_COURSE_NAME, course);
+        contentValues.put(COL_3_PROFESSOR, professor);
+        contentValues.put(COL_4_UNITS, units);
+        contentValues.put(COL_5_START_TIME, start);
+        contentValues.put(COL_6_END_TIME, end);
+        contentValues.put(COL_7_DAYS, days);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
--- app/src/main/java/com/example/studentassistantapplication/addClass.java
+++ app/src/main/java/com/example/studentassistantapplication/addClass.java
@@ -3,29 +3,57 @@ package com.example.studentassistantapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class addClass extends AppCompatActivity {
 
-    ClassDatabaseHelper myDb = new ClassDatabaseHelper(this);
+    ClassDatabaseHelper myDb;
     EditText addCourseName, addProfessor, addUnits, addStart, addEnd, addDays;
     Button subBut;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_class);
+        myDb = new ClassDatabaseHelper(this);
 
-        addCourseName = (EditText)findViewById(R.id.add_class_name);
-        addProfessor = (EditText)findViewById(R.id.add_professor;
-        addUnits = (EditText)findViewById(R.id.add_unit);
-        addStart = (EditText)findViewById(R.id.add_start_time);
-        addEnd = (EditText)findViewById(R.id.add_end_time);
-       // addDays = (EditText)findViewById(R.id.); Need to work out how to store days
-        subBut = (Button)findViewById(R.id.submit);
+        addCourseName = (EditText) findViewById(R.id.add_class_name);
+        addProfessor = (EditText) findViewById(R.id.add_professor);
+        addUnits = (EditText) findViewById(R.id.add_unit);
+        addStart = (EditText) findViewById(R.id.add_start_time);
+        addEnd = (EditText) findViewById(R.id.add_end_time);
+        addDays = (EditText) findViewById(R.id.add_days);
+        subBut = (Button) findViewById(R.id.submit);
 
+        AddData();
+
+    }
+
+
+       public void AddData() {
+        subBut.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(
+                                addCourseName.getText().toString(),
+                                addProfessor.getText().toString(),
+                                addUnits.getText().toString(),
+                                addStart.getText().toString(),
+                                addEnd.getText().toString(),
+                                addDays.getText().toString());
+
+                        if (isInserted == true) {
+                            Toast.makeText(addClass.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(addClass.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+        );
     }
 }
